a318c89c3a16fed806d1b350e3142f862669f69d
unknown <johirhaquedipok2@gmail.com>
2022-07-19 19:36:39 +0600

    routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import SignIn from "./Components/Features/Users/SignIn";
+import SignUp from "./Components/Features/Users/SignUp";
+import HomePage from "./Components/Pages/HomePage/HomePage";
 import Footer from "./Components/Pages/shared/Footer";
 import Navbar from "./Components/Pages/shared/Navbar";
-
 function App() {
   return (
     <>
       <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="singup" element={<SignUp />} />
+      </Routes>
+
       <Footer />
     </>
   );
