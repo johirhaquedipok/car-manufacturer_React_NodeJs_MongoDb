880e46e21871dc8256cb2abe0ae2fc371befd1ab
unknown <johirhaquedipok2@gmail.com>
2022-07-19 06:50:16 +0600

    favicon and title

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div>
       <button className="btn">Button</button>
-      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-primary">ThemeChange</button>
       <button className="btn btn-secondary">Button</button>
       <button className="btn btn-accent">Button</button>
       <button className="btn btn-ghost">Button</button>
