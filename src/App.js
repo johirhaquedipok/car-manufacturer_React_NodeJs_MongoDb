76c0be319283383511ac20f0a782c4da5975e75d
unknown <johirhaquedipok2@gmail.com>
2022-07-19 07:20:01 +0600

    navbar and blog page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import Blog from "./Components/Pages/Blog/Blog";
+import Navbar from "./Components/Pages/Home/Navbar";
+
 function App() {
   return (
-    <div>
-      <button className="btn">Button</button>
-      <button className="btn btn-primary">ThemeChange</button>
-      <button className="btn btn-secondary">Button</button>
-      <button className="btn btn-accent">Button</button>
-      <button className="btn btn-ghost">Button</button>
-      <button className="btn btn-link">Button</button>
-    </div>
+    <>
+      <Navbar />
+
+      <Blog />
+    </>
   );
 }
 
---
+++ src/Components/Pages/Blog/Blog.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <div>
+      <p className="text-4xl text-center">Blog page</p>
+    </div>
+  );
+};
+
+export default Blog;
