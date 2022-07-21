9b5afb6d91237c6ea5fd3eb5daebf5823bd52d77
johirhaquedipok <johirhaquedipok2@gmail.com>
2022-07-21 14:50:53 +0600

    form purchase

--- src/Components/Pages/PurchasePage/Purchase-page.js
+++ src/Components/Pages/PurchasePage/Purchase-page.js
@@ -12,6 +12,11 @@ const PurchasePage = () => {
     formState: { errors },
     handleSubmit,
   } = useForm();
+
+  // sing in with email and password
+  const onSubmit = async (data) => {
+    console.log(data);
+  };
   return (
     <div className="flex">
       {/* card 3 */}
@@ -24,76 +29,88 @@ const PurchasePage = () => {
           <h3 class="card-title"> Product Name </h3>
           <p>Fil all the details about the you and the product...</p>
           {/* product details */}
-          {/* order quantity */}
-          <div className="flex items-center p-1">
-            <span className="label-text w-48 text-md">Quantity</span>
-            <div className="flex justify-center w-1/5">
-              <div className="btn btn-sm font-bold">+</div>
-              <input
-                className="mx-2 border text-center w-8"
-                type="text"
-                value="10"
-                min="10"
-              />
-              <div className="btn btn-sm font-bold ">-</div>
+
+          {/* form */}
+
+          <form className="space-y-5" onSubmit={handleSubmit(onSubmit)}>
+            {/* order quantity */}
+            <div className="flex items-center p-1">
+              <span className="label-text w-48 text-md">Quantity</span>
+              <div className="flex justify-center w-1/5">
+                <div className="btn btn-sm font-bold">+</div>
+                <input
+                  className="mx-2 border text-center w-8"
+                  type="number"
+                  value="10"
+                  min="10"
+                  {...register("qty", {
+                    required: true,
+                  })}
+                />
+                <div className="btn btn-sm font-bold ">-</div>
+              </div>
             </div>
-          </div>
 
-          {/* your email */}
-          <div className="space-y-2">
-            <label className="text-sm font-medium   tracking-wide">Email</label>
-            <input
-              className=" w-full text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-green-400"
-              type="email"
-              placeholder="Your Email"
-              {...register("email", {
-                required: true,
-              })}
-            />
-            {errors.email?.type === "required" && (
-              <p className="text-error">Email is required</p>
-            )}
-          </div>
+            {/* your email */}
+            <div className="space-y-2">
+              <label className="text-sm font-medium   tracking-wide">
+                Email
+              </label>
+              <input
+                className=" w-full text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-green-400"
+                type="email"
+                placeholder="Your Email"
+                {...register("email", {
+                  required: true,
+                })}
+              />
+              {errors.email?.type === "required" && (
+                <p className="text-error">Email is required</p>
+              )}
+            </div>
 
-          {/* your phone number*/}
-          <div className="space-y-2">
-            <label className="text-sm font-medium   tracking-wide">
-              Phone No
-            </label>
-            <input
-              className=" w-full text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-green-400"
-              type="phone"
-              placeholder="Your Phone No"
-              {...register("phone", {
-                required: true,
-              })}
-            />
-            {errors.phone?.type === "required" && (
-              <p className="text-error">Phone No is required</p>
-            )}
-          </div>
+            {/* your phone number*/}
+            <div className="space-y-2">
+              <label className="text-sm font-medium   tracking-wide">
+                Phone No
+              </label>
+              <input
+                className=" w-full text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-green-400"
+                type="number"
+                placeholder="Your Phone No"
+                {...register("phone", {
+                  required: true,
+                })}
+              />
+              {errors.phone?.type === "required" && (
+                <p className="text-error">Phone No is required</p>
+              )}
+            </div>
 
-          {/* your address*/}
-          <div className="space-y-2">
-            <label className="text-sm font-medium   tracking-wide">
-              Your Address
-            </label>
-            <input
-              className=" w-full text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-green-400"
-              type="address"
-              placeholder="Your Phone No"
-              {...register("address", {
-                required: true,
-              })}
-            />
-            {errors.address?.type === "required" && (
-              <p className="text-error">Address is required</p>
-            )}
-          </div>
+            {/* your address*/}
+            <div className="space-y-2">
+              <label className="text-sm font-medium   tracking-wide">
+                Your Address
+              </label>
+              <input
+                className=" w-full text-base px-4 py-2 border  border-gray-300 rounded-lg focus:outline-none focus:border-green-400"
+                type="text"
+                placeholder="Your Address"
+                {...register("address", {
+                  required: true,
+                })}
+              />
+              {errors.address?.type === "required" && (
+                <p className="text-error">Address is required</p>
+              )}
+            </div>
 
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Watch</button>
-          </div>
+            <div class="card-actions  justify-center">
+              <button type="submit" class="btn btn-primary btn-block">
+                Purchase
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
